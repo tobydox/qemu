845b37ffd0b11ee79dec350046ec426c4963d323
Tobias Doerffel <tobias.doerffel@gmail.com>
2013-02-06 23:30:31 +0100

    qemu-coroutine: replaced by fully synchronuous logic

--- qemu-coroutine.c
+++ qemu-coroutine.c
@@ -17,59 +17,48 @@
 #include "block/coroutine.h"
 #include "block/coroutine_int.h"
 
+static Coroutine* currentCORO = NULL;
+
+
 Coroutine *qemu_coroutine_create(CoroutineEntry *entry)
 {
-    Coroutine *co = qemu_coroutine_new();
-    co->entry = entry;
-    return co;
+	Coroutine* coro = malloc(sizeof(Coroutine));
+    coro->entry = entry;
+
+    return coro;
 }
 
-static void coroutine_swap(Coroutine *from, Coroutine *to)
+Coroutine *qemu_coroutine_self()
 {
-    CoroutineAction ret;
+    return currentCORO;
 
-    ret = qemu_coroutine_switch(from, to, COROUTINE_YIELD);
-
-    switch (ret) {
-    case COROUTINE_YIELD:
-        return;
-    case COROUTINE_TERMINATE:
-        trace_qemu_coroutine_terminate(to);
-        qemu_coroutine_delete(to);
-        return;
-    default:
-        abort();
-    }
 }
+
 
 void qemu_coroutine_enter(Coroutine *co, void *opaque)
 {
-    Coroutine *self = qemu_coroutine_self();
+	if( co && co->entry)
+	{
+		Coroutine* tmp = currentCORO;
 
-    trace_qemu_coroutine_enter(self, co, opaque);
+		currentCORO = co;
+		co->entry(opaque);
 
-    if (co->caller) {
-        fprintf(stderr, "Co-routine re-entered recursively\n");
-        abort();
-    }
+		free(co);
 
-    co->caller = self;
-    co->entry_arg = opaque;
-    coroutine_swap(self, co);
+		currentCORO = tmp;
+	}
 }
 
+
+bool qemu_in_coroutine(void)
+{
+	return currentCORO != NULL;
+}
+
+#ifndef qemu_coroutine_yield
 void coroutine_fn qemu_coroutine_yield(void)
 {
-    Coroutine *self = qemu_coroutine_self();
-    Coroutine *to = self->caller;
-
-    trace_qemu_coroutine_yield(self, to);
-
-    if (!to) {
-        fprintf(stderr, "Co-routine is yielding to no one\n");
-        abort();
-    }
-
-    self->caller = NULL;
-    coroutine_swap(self, to);
 }
+#endif
+
